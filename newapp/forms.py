46b4cd385e35a48b993bc91c3749cbd6c00acc42
malavikarajeshvikraman <malu.rajesh2000@gmail.com>
2020-12-23 14:08:32 +0530

    New History

--- newapp/forms.py
+++ newapp/forms.py
@@ -2,3 +2,6 @@ from django import forms
 
 class NameForm(forms.Form):
     marks = forms.IntegerField(label='mark')
+
+class MarkForm(forms.Form):
+    totmarks = forms.IntegerField(label='totmarks')
--- newapp/models.py
+++ newapp/models.py
@@ -6,3 +6,15 @@ from django.db import models
 class history(models.Model):
     rno=models.IntegerField(default=0)
     marks=models.IntegerField(default=0)
+    name=models.CharField(max_length=100,default='')
+    clname=models.CharField(max_length=100,default='')
+    imageurl=models.CharField(max_length=100,default='')
+    tmarks=models.IntegerField(default=0)
+
+class Totalmarks(models.Model):
+    rno=models.IntegerField(default=0)
+    totmarks=models.IntegerField(default=0)
+    name=models.CharField(max_length=100,default='')
+    clname=models.CharField(max_length=100,default='')
+
+
