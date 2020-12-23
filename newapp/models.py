f96afddf3334da99eadbcec19f269f0ad58fe89f
malavikarajeshvikraman <malu.rajesh2000@gmail.com>
2020-12-23 11:28:26 +0530

    History 1

--- newapp/models.py
+++ newapp/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 # Create your models here.
 class history(models.Model):
-    url=models.CharField(max_length=200)
-    data=models.CharField(max_length=2500)
+    rno=models.IntegerField(default=0)
+    marks=models.IntegerField(default=0)
